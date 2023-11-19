3249616dce9809dceb17afbe92d0280c694b1c96
Daniel Zapata <DZAPATAR30@GMAIL.COM>
2023-11-19 15:10:51 -0500

    new

---
+++ Paginas/Scripts/Categoria.js
@@ -0,0 +1,35 @@
+﻿
+jQuery(function () {
+    $("#dvMenu").load("../Paginas/Menu.html")
+
+    $("#btnInsertar").on("click", function () {
+        InsertarCategoria()
+    })
+})
+
+async function InsertarCategoria() {
+    let descripcion = $("#txtDescripcionCatergoria").val();
+
+    let Categoria = {
+        CategoryDescription: descripcion
+    }
+
+    try {
+        const Respuesta = await fetch("http://localhost:44385/api/categories/create",
+            {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(categoria)
+            });
+        const Rpta = await Respuesta.json();
+        //Se presenta la respuesta en el div mensaje
+        $("#dvMensaje").html(Rpta);
+    }
+    catch (error) {
+        //Se presenta la respuesta en el div mensaje
+        $("#dvMensaje").html(error);
+    }
+}
